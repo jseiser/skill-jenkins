23f2f45219aaea438b5d4623657e2980c190f80d
Justin <justin.seiser@rackspace.com>
2019-06-03 09:28:22 -0400

    first jobs test

--- __init__.py
+++ __init__.py
@@ -5,8 +5,6 @@ import aiohttp
 
 
 class JenkinsSkill(Skill):
-    # being Matching Functions
-
     async def _get_deployments(self):
         sites = self.config["sites"]
         return_text = f"*Jenkins Deployments*\n"
@@ -14,8 +12,30 @@ class JenkinsSkill(Skill):
             return_text = f"{return_text}```Deployment: {site} URL: {self.config['sites'][site]['url']}```\n"
         return return_text
 
+    async def _get_jobs(self, deployment):
+        auth = aiohttp.BasicAuth(
+            login=self.config["sites"][deployment]["username"],
+            password=self.config["sites"][deployment]["password"],
+        )
+        timeout = aiohttp.ClientTimeout(total=60)
+        api_url = f"{self.config['sites'][deployment]['url']}/api/json"
+
+        async with aiohttp.ClientSession(auth=auth, timeout=timeout) as session:
+            async with session.get(api_url) as resp:
+                data = await resp.json()
+                return data["jobs"]
+
+    # Matching Functions
+
     @match_regex(r"^jenkins list deployments$")
     async def list_inventory(self, message):
         deployments = await self._get_deployments()
 
         await message.respond(f"{deployments}")
+
+    @match_regex(r"^jenkins (?P<deployment>\w+-\w+|\w+) list jobs$")
+    async def get_jobs(self, message):
+        deployment = message.regex.group("deployment")
+        jobs = await self._get_jobs(deployment)
+
+        await message.respond(f"{jobs}")
